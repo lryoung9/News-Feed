367f86e1696952436478b1ca49e62eb37e4aff11
Lydia Young <lydiaryoung@gmail.com>
2018-03-16 22:30:33 -0600

    set to deploy

--- server.js
+++ server.js
@@ -34,15 +34,15 @@ mongoose.connect(MONGODB_URI, {
 
 // Routes
 
-// A GET route for scraping the echojs website
+// A GET route for scraping the website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("http://www.cracked.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("div.content-card").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
