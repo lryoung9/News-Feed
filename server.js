76f98cf69f70ed8af4898e3e0a80826e8b9b6a45
Lydia Young <lydiaryoung@gmail.com>
2018-03-20 09:57:46 -0600

    pushing changes that weren't saved before the last push

--- server.js
+++ server.js
@@ -50,18 +50,21 @@ app.get("/scrape", function(req, res) {
       result.title = $(this)
         .children.children("a")
         .text();
+      console.log(`Title: ${result.title}`)
       result.link = $(this)
         .parent("a")
         .attr("href");
+      console.log(`Link: ${result.link}`)
       result.summary = $(this)
         .children("p")
         .text();
+      console.log(`Summary: ${result.sumamry}`)
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
-        .then(function(dbArticle) {
+        .then(function(dbArticles) {
           // View the added result in the console
-          console.log(dbArticle);
+          console.log(dbArticles);
         })
         .catch(function(err) {
           // If an error occurred, send it to the client
@@ -76,11 +79,11 @@ app.get("/scrape", function(req, res) {
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
-  // TODO: Finish the route so it grabs all of the articles
+  // Route so it grabs all of the articles
   db.Article.find({})
-  .then(function(dbUser) {
-    // If all Users are successfully found, send them back to the client
-    res.json(dbUser);
+  .then(function(dbArticles) {
+    // If all articles are successfully found, send them back to the client
+    res.json(dbArticles);
   })
   .catch(function(err) {
     // If an error occurs, send the error back to the client
@@ -90,34 +93,31 @@ app.get("/articles", function(req, res) {
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
-  // TODO
-  // ====
-  // Finish the route so it finds one article using the req.params.id,
+  // Route so it finds one article using the req.params.id,
   db.Article.find({ _id: req.params.id })
   // and run the populate method with "note",
   .populate("note")
   // then responds with the article with the note included
-    .then(function(dbArticle) {
-      res.json(dbArticle);
+    .then(function(dbArticles) {
+      res.json(dbArticles);
     })
     .catch(function(err) {
+      // if there's an error, send it to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
-  // TODO
-  // ====
   // save the new note that gets posted to the Notes collection
   // then find an article from the req.params.id
   // and update it's "note" property with the _id of the new note
   db.Article.create(req.body)
-    .then(function(dbArticle) {
+    .then(function(dbArticles) {
       // If a Note was created successfully, find one User (there's only one) and push the new Note's _id to the User's `notes` array
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.User.findOneAndUpdate({}, { $push: { notes: dbArticle._id } }, { new: true });
+      return db.User.findOneAndUpdate({}, { $push: { notes: dbArticles._id } }, { new: true });
     })
     .then(function(dbUser) {
       // If the User was updated successfully, send it back to the client
